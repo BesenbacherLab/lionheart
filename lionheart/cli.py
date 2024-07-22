a7d037837fb7a33b647e8d98022e8ec68805e95f
Ludvig <ludvig@clin.au.dk>
2024-07-22 13:58:11 +0200

    Improves CLI documentation

--- lionheart/cli.py
+++ lionheart/cli.py
@@ -1,36 +1,51 @@
 import re
 import argparse
 from rich_argparse import RawTextRichHelpFormatter
-from rich.console import Console
-from rich.text import Text
-from rich.panel import Panel
 from lionheart.commands import collect_samples, extract_features, predict, train_model
 
 
-# Add styles for bold and italic text
-RawTextRichHelpFormatter.styles["argparse.bold"] = "bold"
-RawTextRichHelpFormatter.styles["argparse.italic"] = "italic"
-RawTextRichHelpFormatter.styles["argparse.underline"] = "underline"
-RawTextRichHelpFormatter.styles["argparse.group_header"] = (
-    RawTextRichHelpFormatter.styles["argparse.groups"]
-)
+# Add styles
+colors = {
+    "red": "#d32c30",
+    "yellow": "#efa12d",
+    "light_yellow": "#f3b65b",
+    "dark_orange": "#d9831c",
+    "blue": "#1075ee",
+    "green": "#4ca526",
+    "light_red": "#f08a8a",  # "#ed8282" # "#ea8080" # "#ff6b6f",
+}
 
+styles = {
+    "color_red": ("cr", colors["red"]),
+    "color_light_red": ("clr", colors["light_red"]),
+    "color_yellow": ("cy", colors["yellow"]),
+    "color_light_yellow": ("cly", colors["light_yellow"]),
+    "color_dark_orange": ("cdo", colors["dark_orange"]),
+    "bold": ("b", "bold"),
+    "italic": ("i", "italic"),
+    "underline": ("u", "underline"),
+    "groups": ("h1", colors["light_yellow"]),
+    "args": (None, colors["light_red"]),
+}
 
-# Define highlight regex patterns for bold and italic, ensuring the markers are not included in the captured groups
-RawTextRichHelpFormatter.highlights.append(r"\<b\>(?P<bold>.+?)\</b\>")
-RawTextRichHelpFormatter.highlights.append(r"\<i\>(?P<italic>.+?)\</i\>")
-RawTextRichHelpFormatter.highlights.append(r"\<u\>(?P<underline>.+?)\</u\>")
-RawTextRichHelpFormatter.highlights.append(r"\<h1\>(?P<group_header>.+?)\</h1\>")
+style_tags = []
+for style_name, (style_tag, style) in styles.items():
+    RawTextRichHelpFormatter.styles[f"argparse.{style_name}"] = style
+    if style_tag is not None:
+        RawTextRichHelpFormatter.highlights.append(
+            r"\<" + style_tag + r"\>(?P<" + style_name + r">.+?)\</" + style_tag + r"\>"
+        )
+        style_tags.append(style_tag)
 
 
 # Custom formatter class to remove markers after formatting
 class CustomRichHelpFormatter(RawTextRichHelpFormatter):
+    TAGS = style_tags
+
     def _strip_html_tags(self, text):
-        # Remove bold markers
-        text = re.sub(r"\</?b\>", "", text)
-        text = re.sub(r"\</?i\>", "", text)
-        text = re.sub(r"\</?u\>", "", text)
-        text = re.sub(r"\</?h1\>", "", text)
+        for tag_text in CustomRichHelpFormatter.TAGS:
+            # Remove bold markers
+            text = re.sub(r"\</?" + tag_text + r"\>", "", text)
         return text
 
     def format_help(self):
@@ -46,11 +61,47 @@ class CustomRichHelpFormatter(RawTextRichHelpFormatter):
         return self._strip_html_tags(formatted_text)
 
 
+lion_ascii = """<b><cy>    :::::       </cy><cr>                </cr><cy> =##=:.   </cy></b>
+<b><cy>   -:</cy><cly>.</cly><cy>:-:...    </cy><cr>               </cr><cy>       -.  </cy></b>
+<b><cly>  :::</cly><cy>- ......:                </cr><cy>        -:  </cy></b>
+<b><cly> :.  </cly><cy>-  ..    ::</cy><cr>.........</cr><cy>.....:..  ..:-.  </cy></b>
+<b><cly> :-.</cly><cy>:  .: . ..</cy><cr>.             </cr><cy>  -</cy><cdo>.</cdo><cy>:::...    </cy></b>
+<b><cy>   .:  .: :</cy><cr>:              </cr><cy>    -           </cy></b>
+<b><cy>     :.: .. </cy><cr>:           </cr><cy>      .:          </cy></b>
+<b><cy>      : :. .</cy><cr>:       ..-</cr><cy>. ::    -          </cy></b>
+<b><cy>       -   :</cy><cr>:.-.....</cr><cy>   :  -...  :.        </cy></b>
+<b><cy>       -  - - :.        :  -  .:  :       </cy></b>
+<b><cy>       : :   :.:.:     .: :.   :.:        </cy></b>
+<b><cy>    .-:.:       .:    :...   .::..        </cy></b>
+
+"""
+
+lionheart_ascii = """<b>........................................  </b>
+<b><cy>_    _ ____ _  _</cy><cr> _  _ ____ ____ ____ ___ </cr></b>
+<b><cy>|    | |  | |\ |</cy><cr> |__| |___ |__| |__/  |  </cr></b>
+<b><cy>|___ | |__| | \|</cy><cr> |  | |___ |  | |  \  |  </cr></b>
+
+<b>........................................  </b>
+"""
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description="""<b>LIONHEART Cancer Detector</b>\n\nDetect Cancer from whole genome sequenced plasma cell-free DNA.\n\n
-Start by <i>extracting</i> the features from a BAM file. Then <i>predict</i> whether a sample is from a cancer patient or not.\n\n
-Easily <i>train</i> a new model on your own data or perform <i>cross-validation</i> to compare against the paper.
+        description=f"""\n                                                                               
+{lion_ascii}                                        
+
+{lionheart_ascii}
+
+<b>L</b>iquid B<b>i</b>opsy C<b>o</b>rrelati<b>n</b>g C<b>h</b>romatin
+Acc<b>e</b>ssibility and cfDN<b>A</b> Cove<b>r</b>age
+Across Cell-<b>T</b>ypes
+.................
+
+Detect Cancer from whole genome sequenced plasma cell-free DNA.
+
+Start by <b>extracting</b> the features from a BAM file. Then <b>predict</b> whether a sample is from a cancer patient or not.
+
+Easily <b>train</b> a new model on your own data or perform <b>cross-validation</b> to compare against the paper.
         """,
         formatter_class=CustomRichHelpFormatter,
     )
@@ -65,7 +116,7 @@ Easily <i>train</i> a new model on your own data or perform <i>cross-validation<
     parser_ef = subparsers.add_parser(
         "extract_features",
         help="Extract features from a BAM file",
-        description="Extract features from a BAM file.",
+        description=f"{lionheart_ascii}\nEXTRACT FEATURES from a BAM file.",
         formatter_class=parser.formatter_class,
     )
     # Delegate the argument setup to the respective command module
@@ -75,7 +126,7 @@ Easily <i>train</i> a new model on your own data or perform <i>cross-validation<
     parser_ps = subparsers.add_parser(
         "predict_sample",
         help="Predict cancer status of a sample",
-        description="Predict whether a sample is cancer or control.",
+        description=f"{lionheart_ascii}\PREDICT whether a sample is cancer or control.",
         formatter_class=parser.formatter_class,
     )
     # Delegate the argument setup to the respective command module
@@ -85,7 +136,7 @@ Easily <i>train</i> a new model on your own data or perform <i>cross-validation<
     parser_cl = subparsers.add_parser(
         "collect",
         help="Collect predictions and/or features across samples",
-        description="Collect predictions and/or extracted features for multiple samples.",
+        description=f"{lionheart_ascii}\COLLECT predictions and/or extracted features for multiple samples.",
         formatter_class=parser.formatter_class,
     )
     # Delegate the argument setup to the respective command module
@@ -95,7 +146,7 @@ Easily <i>train</i> a new model on your own data or perform <i>cross-validation<
     parser_tm = subparsers.add_parser(
         "train_model",
         help="Train a model on your own data and/or the included features",
-        description="Train a model on your extracted features and/or the included features.",
+        description=f"{lionheart_ascii}\nTRAIN A MODEL on your extracted features and/or the included features.",
         formatter_class=parser.formatter_class,
         epilog=train_model.EPILOG,
     )
@@ -106,8 +157,8 @@ Easily <i>train</i> a new model on your own data or perform <i>cross-validation<
     parser_cv = subparsers.add_parser(
         "cross_validate",
         help="Cross-validate the cancer detection model on your own data and/or the included features",
-        description="Perform nested leave-one-dataset-out (or classic) cross-validation "
-        "with your extracted features and/or the included features.",
+        description=f"{lionheart_ascii}\nCROSS-VALIDATE your features with nested leave-one-dataset-out (or classic) cross-validation. "
+        "Use your extracted features and/or the included features.",
         formatter_class=parser.formatter_class,
     )
     # Delegate the argument setup to the respective command module
