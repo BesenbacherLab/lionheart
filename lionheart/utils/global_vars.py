ce6f862d9d8372c51059a45d872a006b2fde25c8
Ludvig <ludvig@clin.au.dk>
2024-08-26 18:16:01 +0200

    Updates list of included models

--- lionheart/utils/global_vars.py
+++ lionheart/utils/global_vars.py
@@ -2,6 +2,6 @@
 JOBLIB_VERSION = "1.2.0"
 
 # NOTE: Remember to set the newest model first in the list!
-INCLUDED_MODELS = ["detect_cancer__004__24_08_24", "subtype_cancers__004__24_08_24"]
+INCLUDED_MODELS = ["detect_cancer__004__24_08_24", "subtype_cancers__004__26_08_24"]
 
 REPO_URL = "https://github.com/besenbacherlab/lionheart"
