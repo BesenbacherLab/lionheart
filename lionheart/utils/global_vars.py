dd27f1b333135b1d1ab28a75e64245adb7c3e0d2
Ludvig <ludvig@clin.au.dk>
2024-12-01 02:32:35 +0100

    Fixes model name

--- lionheart/utils/global_vars.py
+++ lionheart/utils/global_vars.py
@@ -1,6 +1,6 @@
 # NOTE: Remember to set the newest model first in the list!
 INCLUDED_MODELS = [
-    "detect_cancer__007__20_09_24",
+    "detect_cancer__001__25_11_24",
 ]
 
 # Validate model names
