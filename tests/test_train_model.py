5156e7b88950b1dbe459169265d63a6bf9be41fb
Ludvig <ludvig@clin.au.dk>
2024-12-02 16:06:40 +0100

    Adds test of model training

---
+++ tests/test_train_model.py
@@ -0,0 +1,122 @@
+import json
+import numpy as np
+import pandas as pd
+from utipy import mk_dir
+
+# """
+# examples.add_example(
+#     description="Simple example using defaults:",
+#     example="""--dataset_paths path/to/dataset_1/feature_dataset.npy path/to/dataset_2/feature_dataset.npy
+# --meta_data_paths path/to/dataset_1/meta_data.csv path/to/dataset_2/meta_data.csv
+# --out_dir path/to/output/directory
+# --use_included_features
+# --resources_dir path/to/resource/directory""",
+# )
+# # TODO Implement this:
+# examples.add_example(
+#     description="Train a model on a single dataset. This uses within-dataset cross-validation for hyperparameter optimization:",
+#     example="""--dataset_paths path/to/dataset/feature_dataset.npy
+# --meta_data_paths path/to/dataset/meta_data.csv
+# --out_dir path/to/output/directory
+# --resources_dir path/to/resource/directory""",
+# )
+
+# """
+
+
+def test_train_model_only_shared_features(run_cli, tmp_path, resource_path):
+    sample_dir = tmp_path / "test_sample"
+    mk_dir(sample_dir / "dataset")
+    output_subdir = "model_output"
+
+    command_args = [
+        "lionheart",
+        "train_model",
+        "--use_included_features",
+        "--resources_dir",
+        resource_path,
+    ]
+    generated_files, output_dir = run_cli(
+        command_args=command_args,
+        tmp_path=tmp_path,
+        output_subdir=output_subdir,
+    )
+
+    # Expected files
+    expected_files = [
+        "predictions.csv",
+        "model.joblib",
+        "confusion_matrices.json",
+        "evaluation_scores.csv",
+        "feature_contributions.csv",
+        "feature_effects_on_probability.csv",
+        "probability_densities.csv",
+        "ROC_curves.json",
+        "threshold_versions.txt",
+        "training_info.json",
+        "warnings.csv",
+    ]
+
+    # Check that expected files are generated
+    for expected_file in expected_files:
+        assert (
+            expected_file in generated_files
+        ), f"Expected file {expected_file} not found."
+
+    # Check prediction
+    with open(tmp_path / output_subdir / "training_info.json") as f:
+        training_info = json.load(f)
+
+    expected_training_info = {
+        "Task": "Cancer Detection",
+        "Modeling Task": "binary_classification",
+        "Package Versions": {
+            "lionheart": "0.1.0",
+            "generalize": "0.1.0",
+            "joblib": "1.2.0",
+            "sklearn": "1.0.2",
+            "Min. Required lionheart": "N/A",
+        },
+        "Labels": {
+            "Labels to Use": ["0_Control(control)", "1_Cancer(cancer)"],
+            "Positive Label": 1,
+            "New Label Index to New Label": {"0": "Control", "1": "Cancer"},
+            "New Label to New Label Index": {"Control": 0, "Cancer": 1},
+        },
+        "Data": {
+            "Shape": [2334, 489],
+            "Target counts": {"0": 1083, "1": 1251},
+            "Datasets": {
+                "Names": [
+                    "Cristiano 2019",
+                    "Mathios 2021",
+                    "Mathios 2021 Validation",
+                    "Prostate Cancer (Aarhus cohorts)",
+                    "Nordentoft 2024 and Frydendahl 2024",
+                    "GECOCA",
+                    "Endoscopy II",
+                    "Jiang 2015",
+                ],
+                "Number of Samples": {
+                    "Cristiano 2019": 474,
+                    "Endoscopy II": 256,
+                    "GECOCA": 168,
+                    "Jiang 2015": 112,
+                    "Mathios 2021": 270,
+                    "Mathios 2021 Validation": 431,
+                    "Nordentoft 2024 and Frydendahl 2024": 274,
+                    "train_only(Prostate Cancer (Aarhus cohorts))": 349,
+                },
+            },
+        },
+    }
+
+    assert list(expected_training_info.keys()) == list(training_info.keys())
+
+    for key in training_info.keys():
+        if key == "Package Versions":
+            continue
+        assert training_info[key] == expected_training_info[key]
+
+    predictions = pd.read_csv(tmp_path / output_subdir / "predictions.csv")
+    predictions.iloc[0, 0] = 0.11913294
