e8dc3f1945c552e307da69a8210b802401394dc9
Ludvig <ludvig@clin.au.dk>
2024-12-02 15:40:46 +0100

    Checks if env var is set
    Fixes data type
    
    Adds missing arg
    
    Creates missing folder
    
    Fixes function call
    
    Fixes score array size
    
    Passes missing dir arg
    
    Tests expected prediction
    
    Adds rounding to tests

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -5,22 +5,27 @@ from utipy import mk_dir
 
 def test_predict(run_cli, tmp_path, resource_path, lionheart_features):
     sample_dir = tmp_path / "test_sample"
-    mk_dir(sample_dir)
+    mk_dir(sample_dir / "dataset")
     output_subdir = "prediction_output"
 
     scores = np.expand_dims(np.array(lionheart_features), 0)
-    assert scores.shape == [1, 489]
-    np.save(sample_dir / "dataset" / "feature_dataset.npy")
+    scores = np.concatenate([scores for _ in range(10)], axis=0)
+    assert scores.shape == (10, 489)
+    np.save(sample_dir / "dataset" / "feature_dataset.npy", scores)
 
     command_args = [
         "lionheart",
-        "predict",
+        "predict_sample",
         "--sample_dir",
         tmp_path / "test_sample",
         "--resources_dir",
         resource_path,
     ]
-    generated_files, output_dir = run_cli(command_args, tmp_path)
+    generated_files, output_dir = run_cli(
+        command_args=command_args,
+        tmp_path=tmp_path,
+        output_subdir=output_subdir,
+    )
 
     # Expected files
     expected_files = ["prediction.csv", "README.txt"]
@@ -32,6 +37,8 @@ def test_predict(run_cli, tmp_path, resource_path, lionheart_features):
         ), f"Expected file {expected_file} not found."
 
     # Check prediction
-    print(pd.read_csv(tmp_path / output_subdir))
+    prediction = pd.read_csv(tmp_path / output_subdir / "prediction.csv")
+    print(prediction)
 
-    assert False
+    assert prediction["Prediction"].tolist() == ["Cancer"] * 6
+    assert np.round(prediction["P(Cancer)"], decimals=4).tolist() == [0.9932] * 6
