c6973e7810a97f61bb1cbcee389d8b301a123715
Ludvig <ludvig@clin.au.dk>
2024-12-03 17:58:45 +0100

    Adds CV test with single dataset

--- tests/test_cross_validate.py
+++ tests/test_cross_validate.py
@@ -78,102 +78,79 @@ def test_cross_validate_three_shared_datasets(run_cli, tmp_path, resource_path):
         np.unique(splits_summary["Fold"]),
         ["Cristiano 2019", "EndoscopyII", "Jiang 2015"],  # alphabetical order
     )
+    assert splits_summary.iloc[0, "AUC"] == 0.817962
+    assert splits_summary.iloc[4, "AUC"] == 0.654003
 
-    assert False
 
+def test_cross_validate_single_shared_datasets(run_cli, tmp_path, resource_path):
+    sample_dir = tmp_path / "test_sample"
+    mk_dir(sample_dir / "dataset")
+    output_subdir = "model_output"
 
-# def test_train_model_one_shared_dataset(run_cli, tmp_path, resource_path):
-#     sample_dir = tmp_path / "test_sample"
-#     mk_dir(sample_dir / "dataset")
-#     output_subdir = "model_output"
+    command_args = [
+        "lionheart",
+        "cross_validate",
+        "--dataset_paths",
+        resource_path / "shared_features" / "Cristiano" / "feature_dataset.npy",
+        "--meta_data_paths",
+        resource_path / "shared_features" / "Cristiano" / "meta_data.csv",
+        "--dataset_names",
+        "Cristiano 2019",
+        "--resources_dir",
+        resource_path,
+        "--k_outer",
+        "5",
+        "--k_inner",
+        "5",
+        "--pca_target_variance",
+        "0.996",
+        "0.997",
+        "--lasso_c",
+        "0.04",
+        "0.05",
+    ]
+    generated_files, output_dir = run_cli(
+        command_args=command_args,
+        tmp_path=tmp_path,
+        output_subdir=output_subdir,
+    )
 
-#     command_args = [
-#         "lionheart",
-#         "train_model",
-#         "--dataset_paths",
-#         resource_path / "shared_features" / "Cristiano" / "feature_dataset.npy",
-#         "--meta_data_paths",
-#         resource_path / "shared_features" / "Cristiano" / "meta_data.csv",
-#         "--dataset_names",
-#         "Cristiano 2019",
-#         "--resources_dir",
-#         resource_path,
-#         "--pca_target_variance",
-#         "0.996",
-#         "0.997",
-#         "--lasso_c",
-#         "0.04",
-#         "0.05",
-#     ]
-#     generated_files, output_dir = run_cli(
-#         command_args=command_args,
-#         tmp_path=tmp_path,
-#         output_subdir=output_subdir,
-#     )
+    # print(generated_files)
 
-#     # Expected files
-#     expected_files = [
-#         "predictions.csv",
-#         "model.joblib",
-#         "confusion_matrices.json",
-#         "evaluation_scores.csv",
-#         "feature_contributions.csv",
-#         "feature_effects_on_probability.csv",
-#         "probability_densities.csv",
-#         "ROC_curves.json",
-#         "threshold_versions.txt",
-#         "training_info.json",
-#         "warnings.csv",
-#     ]
+    # Expected files
+    expected_files = [
+        "warnings.csv",
+        "predictions.csv",
+        "evaluation_scores.csv",
+        "evaluation_summary.csv",
+        "confusion_matrices.json",
+        "total_confusion_matrices.json",
+        "ROC_curves.json",
+        "splits_summary.csv",
+        "inner_results.csv",
+        "best_coefficients.csv",
+        "inner_cv_Score_HP_C.png",
+        "inner_cv_Score_HP_Target_Variance.png",
+        "inner_cv_Time_HP_C.png",
+        "inner_cv_Time_HP_Target_Variance.png",
+    ]
 
-#     # Check that expected files are generated
-#     for expected_file in expected_files:
-#         assert (
-#             expected_file in generated_files
-#         ), f"Expected file {expected_file} not found."
+    # Check that expected files are generated
+    for expected_file in expected_files:
+        assert (
+            expected_file in generated_files
+        ), f"Expected file {expected_file} not found."
 
-#     # Check prediction
-#     with open(tmp_path / output_subdir / "training_info.json") as f:
-#         training_info = json.load(f)
+    predictions = pd.read_csv(tmp_path / output_subdir / "predictions.csv")
+    print(predictions.iloc[0])
+    assert predictions.iloc[0, 0] == 0.525994
 
-#     print(training_info)
-
-#     expected_training_info = {
-#         "Task": "Cancer Detection",
-#         "Modeling Task": "binary_classification",
-#         "Package Versions": {
-#             "lionheart": "0.1.0",
-#             "generalize": "0.1.0",
-#             "joblib": "1.2.0",
-#             "sklearn": "1.0.2",
-#             "Min. Required lionheart": "N/A",
-#         },
-#         "Labels": {
-#             "Labels to Use": ["0_Control(control)", "1_Cancer(cancer)"],
-#             "Positive Label": 1,
-#             "New Label Index to New Label": {"0": "Control", "1": "Cancer"},
-#             "New Label to New Label Index": {"Control": 0, "Cancer": 1},
-#         },
-#         "Data": {
-#             "Shape": [474, 489],
-#             "Target counts": {"0": 244, "1": 230},
-#             "Datasets": {
-#                 "Names": [
-#                     "Cristiano 2019",
-#                 ],
-#                 "Number of Samples": {
-#                     "Cristiano 2019": 474,
-#                 },
-#             },
-#         },
-#     }
-
-#     assert list(expected_training_info.keys()) == list(training_info.keys())
-
-#     for key in training_info.keys():
-#         if key == "Package Versions":
-#             continue
-#         assert training_info[key] == expected_training_info[key]
-
-#     predictions = pd.read_csv(tmp_path / output_subdir / "predictions.csv")
-#     predictions.iloc[0, 0] = 0.11913294
+    splits_summary = pd.read_csv(tmp_path / output_subdir / "splits_summary.csv")
+    print(splits_summary)
+    print(splits_summary["AUC"])
+    npt.assert_equal(
+        np.unique(splits_summary["Fold"]),
+        ["Cristiano 2019", "EndoscopyII", "Jiang 2015"],  # alphabetical order
+    )
+    assert splits_summary.iloc[0, "AUC"] == 0.817962
+    assert splits_summary.iloc[4, "AUC"] == 0.654003
