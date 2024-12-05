75187cfbe53f16a167e66fa4c2316faa05c898c3
Ludvig <ludvig@clin.au.dk>
2024-12-05 18:24:07 +0100

    Fix use of asserter

--- tests/test_predict_sample.py
+++ tests/test_predict_sample.py
@@ -119,7 +119,7 @@ def test_predict_with_custom_model_and_roc(
         prediction["Prediction"].tolist() == ["Cancer"] * 12
     )  # Also tests size of data frame
 
-    npt.assert_almost_equal(
+    npt.assert_equal(
         prediction.loc[[0, 6], "ROC Curve"].tolist(),
         ["Average (training data)", "Custom 0"],
         decimal=4,
