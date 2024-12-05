a5eaec535c0d12b6c5d14848e0b5a818210ee648
Ludvig <ludvig@clin.au.dk>
2024-12-05 17:37:16 +0100

    Adds single dataset, custom model test for extract_roc

--- tests/test_extract_roc.py
+++ tests/test_extract_roc.py
@@ -36,3 +36,41 @@ def test_extract_roc_shared_resources(
     print(roc)
 
     assert False
+
+
+def test_extract_roc_single_dataset_custom_model(
+    run_cli,
+    tmp_path,
+    resource_path,
+):
+    output_subdir = "roc_output"
+
+    command_args = [
+        "lionheart",
+        "extract_roc",
+        "--dataset_paths",
+        resource_path / "shared_features" / "GECOCA" / "feature_dataset.npy",
+        "--meta_data_paths",
+        resource_path / "shared_features" / "GECOCA" / "feature_dataset.npy",
+        "--custom_model_dir",
+        resource_path / "models" / "detect_cancer__001__25_11_24",
+    ]
+    generated_files, output_dir = run_cli(
+        command_args=command_args,
+        tmp_path=tmp_path,
+        output_subdir=output_subdir,
+    )
+
+    # Expected files
+    expected_files = ["ROC_curves.json"]
+
+    # Check that expected files are generated
+    for expected_file in expected_files:
+        assert (
+            expected_file in generated_files
+        ), f"Expected file {expected_file} not found."
+
+    roc = ROCCurves.load(tmp_path / output_subdir / "ROC_curves.json").to_dict()
+    print(roc)
+
+    assert False
