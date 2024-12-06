1d9b9643ac638d7a2ab3e36e8973f17a79eb31b3
Ludvig <ludvig@clin.au.dk>
2024-12-06 23:42:00 +0100

    Weird debugging

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -114,7 +114,7 @@ def test_validate_reproducibility(run_cli, tmp_path, resource_path):
 
     assert len(prediction) == 2106
 
-    print(prediction.loc[:3, "Prediction"].tolist())
+    print(prediction.loc[:3, "Prediction"])
     assert prediction.loc[:3, "Prediction"].tolist() == ["No Cancer"] * 3
     npt.assert_almost_equal(
         prediction.loc[:3, "P(Cancer)"],
