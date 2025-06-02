e2716bb2af9301982bb713b2adf73c12c403eef3
Ludvig <ludvig@clin.au.dk>
2025-06-02 23:22:14 +0200

    Sets PCA hparams grid for full model

--- lionheart/utils/global_vars.py
+++ lionheart/utils/global_vars.py
@@ -18,7 +18,7 @@ JOBLIB_VERSION = "1.4.2"
 REPO_URL = "https://github.com/besenbacherlab/lionheart"
 
 PCA_TARGET_VARIANCE_OPTIONS = [0.993, 0.994, 0.995, 0.996, 0.997]
-PCA_TARGET_VARIANCE_FM_OPTIONS = [0.992, 0.993, 0.994, 0.995, 0.996] # for full model with +1 dataset
+PCA_TARGET_VARIANCE_FM_OPTIONS = [0.987, 0.988, 0.989, 0.990, 0.991, 0.992, 0.993, 0.994] # for full model with +1 dataset
 LASSO_C_OPTIONS = [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2]
 PCA_TARGET_VARIANCE_OPTIONS_STRING = (
     "`" + " ".join([str(x) for x in PCA_TARGET_VARIANCE_OPTIONS]) + "`"
